53340a6722d08085dd4d6aad163ea1ba32cd953d
TuhakarainaB <Bruce.Tuhakaraina@toiohomai.ac.nz>
2023-08-17 13:18:45 +1200

    Styled Nav and Truck components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import data from './data.json';
+import Truck from './Truck';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Nav from './Nav';
 
 function App() {
   return (
-    <h1>Bare Bones React App</h1>
+    <Router>
+      <Nav data={data} />
+
+      <Routes>
+        <Route path={"HOME"} element={<Default />} />
+        {
+          data.map(
+            truck => (
+              <Route key={truck.model} path={`${truck.model}`} element={<Truck truck={truck} />} />
+            )
+          )
+        }
+      </Routes>
+
+    </Router>
   );
 }
 
 export default App;
+
+function Default()
+{
+  return(
+    <div class="container component">
+    <h1>Welcome to the Comp.6210 Practise react application</h1>
+    <p>Use the menu above to navigate the application.</p>
+    </div>
+  )
+}
